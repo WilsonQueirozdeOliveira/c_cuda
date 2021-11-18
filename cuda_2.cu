9503e1c5cf0800f5c35751ff5358338b51edeb79
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2021-11-18 01:20:38 -0300

    rm old book.h

--- cuda_2.cu
+++ cuda_2.cu
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "book.h"
 
 __global__ void add( int a, int b, int *c ) {
 	*c = a + b;
@@ -8,14 +7,11 @@ __global__ void add( int a, int b, int *c ) {
 int main (void) {
 	int c;
 	int *dev_c;
-	HANDLE_ERROR( cudaMalloc( (void**)&dev_c, sizeof(int) ) );
+	cudaMalloc( (void**)&dev_c, sizeof(int) );
 
 	add<<<1,1>>>(2, 7, dev_c);
 
-	HANDLE_ERROR( cudaMemcpy( &c,
-				dev_c,
-				sizeof(int),
-				cudaMemcpyDeviceToHost ) );
+	cudaMemcpy( &c, dev_c, sizeof(int), cudaMemcpyDeviceToHost );
 	printf( "2 + 7 = %d\n", c);
 	cudaFree( dev_c );
 
